1e83f4710c860ef092e6f2533e8cdadfd802f531
Hassan Javaid <scuderia.hj@gmail.com>
2024-07-20 18:55:13 +0500

    Task 2 completed

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
+  name: "cart",
+  initialState,
   reducers: {
     addItem: (state, action) => {
-    
+      const { id, name, imageUrl, cost } = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ id, name, imageUrl, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      const id = action.payload;
+      state.items = state.items.filter((item) => item.id !== id);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { plant, diff } = action.payload;
+      let itemToUpdate = state.items.find((item) => item.id === plant.id);
+      if (itemToUpdate && itemToUpdate.quantity + diff > 0) {
+        itemToUpdate.quantity += diff;
+      }
     },
   },
 });
